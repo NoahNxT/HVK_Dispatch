e843e1e3795e6afd1ea05602b526ee5fabbe6023
Noah Gillard <noah.gillard@student.kdg.be>
2020-04-23 16:30:29 +0200

    Badges and title hulppost

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,65 +1,298 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container_category">
+    <div class="row align-content-center">
+        
+
+    </div>
+</div>
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
+        <div class="col">
+                <div class="card-header"  id="category">Hulppost 1</div>
+            </div>
+
+            <div class="col">
+                <div class="card-header"  id="category">Hulppost 2</div>
+            </div>
+
+            <div class="col">
+                <div class="card-header"  id="category">Backstage</div>
+            </div>
+
+            <div class="col">
+                <div class="card-header"  id="category">Smalland</div>
+            </div>
+
+            <div class="col">
+                <div class="card-header"  id="category">UZA</div>
+            </div>
+    </div>
+
+    <div class="row">
         <div class="col">
             
                 <div class="card-header">Interventie ploeg 1</div>
 
-                <div class="card-body">
-                    
+                <div class="card-body border">
+                    <div class="row">
+                        <div class="col-4">
+                            <img class="img-thumbnail" src="images/icons/medical-profile-icon.png"></img>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Status
+                                        </button>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">Beschikbaar</a>
+                                            <a class="dropdown-item" href="#">Vertrokken</a>
+                                            <a class="dropdown-item" href="#">Ter Plaatse</a>
+                                            <a class="dropdown-item" href="#">Transport</a>
+                                            <a class="dropdown-item" href="#">Transport aankomst</a>
+                                            <a class="dropdown-item" href="#">Radiofonisch</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Niet Beschikbaar</a>
+                                        </div>
+                                        </div>
+                                </div>
+
+                                <div class="col-auto">
+                                    <div class="form-group row">
+
+                                        <input class="form-control" type="time" value="13:45:00" id="example-time-input">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+
+                
         </div>
-
-
-
-
-
 
         <div class="col">
             
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Interventie ploeg 1</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <div class="card-body border">
+                    <div class="row">
+                        <div class="col-4">
+                            <img class="img-thumbnail" src="images/icons/medical-profile-icon.png"></img>
                         </div>
-                    @endif
 
-                    You are logged in!
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Status
+                                        </button>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">Beschikbaar</a>
+                                            <a class="dropdown-item" href="#">Vertrokken</a>
+                                            <a class="dropdown-item" href="#">Ter Plaatse</a>
+                                            <a class="dropdown-item" href="#">Transport</a>
+                                            <a class="dropdown-item" href="#">Transport aankomst</a>
+                                            <a class="dropdown-item" href="#">Radiofonisch</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Niet Beschikbaar</a>
+                                        </div>
+                                        </div>
+                                </div>
+
+                                <div class="col-auto">
+                                    <div class="form-group row">
+
+                                        <input class="form-control" type="time" value="13:45:00" id="example-time-input">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+
+                
         </div>
 
-
-
-
-
-        
         <div class="col">
             
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Interventie ploeg 1</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <div class="card-body border">
+                    <div class="row">
+                        <div class="col-4">
+                            <img class="img-thumbnail" src="images/icons/medical-profile-icon.png"></img>
                         </div>
-                    @endif
 
-                    You are logged in!
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Status
+                                        </button>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">Beschikbaar</a>
+                                            <a class="dropdown-item" href="#">Vertrokken</a>
+                                            <a class="dropdown-item" href="#">Ter Plaatse</a>
+                                            <a class="dropdown-item" href="#">Transport</a>
+                                            <a class="dropdown-item" href="#">Transport aankomst</a>
+                                            <a class="dropdown-item" href="#">Radiofonisch</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Niet Beschikbaar</a>
+                                        </div>
+                                        </div>
+                                </div>
+
+                                <div class="col-auto">
+                                    <div class="form-group row">
+
+                                        <input class="form-control" type="time" value="13:45:00" id="example-time-input">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+
+                
         </div>
 
-                    
+        <div class="col">
             
+                <div class="card-header">Interventie ploeg 1</div>
 
-        
+                <div class="card-body border">
+                    <div class="row">
+                        <div class="col-4">
+                            <img class="img-thumbnail" src="images/icons/medical-profile-icon.png"></img>
+                        </div>
 
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Status
+                                        </button>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">Beschikbaar</a>
+                                            <a class="dropdown-item" href="#">Vertrokken</a>
+                                            <a class="dropdown-item" href="#">Ter Plaatse</a>
+                                            <a class="dropdown-item" href="#">Transport</a>
+                                            <a class="dropdown-item" href="#">Transport aankomst</a>
+                                            <a class="dropdown-item" href="#">Radiofonisch</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Niet Beschikbaar</a>
+                                        </div>
+                                        </div>
+                                </div>
+
+                                <div class="col-auto">
+                                    <div class="form-group row">
+
+                                        <input class="form-control" type="time" value="13:45:00" id="example-time-input">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                
         </div>
 
-        
+        <div class="col">
+            
+                <div class="card-header">Interventie ploeg 1</div>
+
+                <div class="card-body border">
+                    <div class="row">
+                        <div class="col-4">
+                            <img class="img-thumbnail" src="images/icons/medical-profile-icon.png"></img>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Status
+                                        </button>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">Beschikbaar</a>
+                                            <a class="dropdown-item" href="#">Vertrokken</a>
+                                            <a class="dropdown-item" href="#">Ter Plaatse</a>
+                                            <a class="dropdown-item" href="#">Transport</a>
+                                            <a class="dropdown-item" href="#">Transport aankomst</a>
+                                            <a class="dropdown-item" href="#">Radiofonisch</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Niet Beschikbaar</a>
+                                        </div>
+                                        </div>
+                                </div>
+
+                                <div class="col-auto">
+                                    <div class="form-group row">
+
+                                        <input class="form-control" type="time" value="13:45:00" id="example-time-input">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                
+        </div>
+
+
+
     </div>
 </div>
 @endsection
