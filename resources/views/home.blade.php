e53ed57d42b12b33c1e51832d4d27862e4ba6778
Noah Gillard <noah.gillard@student.kdg.be>
2020-04-25 16:43:54 +0200

    mug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,17 @@
                 <div class="card-footer text-center bg-transparent border-success">Smallandlaan 14a</div>
             </div>
 
+            <div class="card border-success mb-3" style="max-width: 160px;">
+                <div class="card-header text-center alert-success border-success">
+                    <a>MUG</br> <a id="clock">09:55</a></a> 
+                    
+                </div>
+                <div class="card-body text-center text-success">
+                    <img class="img-thumbnail" src="images/icons/mosquito.png"></img> 
+                </div>
+                <div class="card-footer text-center bg-transparent border-success">A12</div>
+            </div>
+
     </div>
 </div>
 @endsection
