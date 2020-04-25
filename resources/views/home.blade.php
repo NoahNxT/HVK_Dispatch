b403b29cf05d8feab446a11ae465db3202a84a86
Noah Gillard <noah.gillard@student.kdg.be>
2020-04-25 13:34:27 +0200

    remove hulpposten

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,11 @@
 </div>
 <div class="container">
     <div class="row">
-        <div class="col-2">
-            <div class="row">
-                <div class=col-12>
-                    <div class="card-header"  id="category">Hulppost 1</div>
-                </div>
-            </div>
-        </div>
         <div class="col">
         <table class="table">
         <thead>
             <tr>
-            <th scope="col">#</th>
+            <th scope="col">Standplaats</th>
             <th scope="col">Ploeg</th>
             <th scope="col">Status</th>
             <th scope="col">Interventie nr</th>
@@ -24,193 +17,176 @@
         </thead>
         <tbody>
             <tr class="table-warning">
-            <td>1</td>
-            <th scope="row">Interventie ploeg 1</th>
+            <td>HP1</td>
+            <th scope="row">Interventie ploeg 1 </br>Interventie ploeg 22 </br>Interventie ploeg 33 </br>Interventie ploeg 44</th>
             <td>Vertrokken</td>
             <td>123456</td>
             <td>Smallandlaan 14a, 2660 Hoboken</td>
             </tr>
             <tr class="table-success">
-            <td>2</td>
+            <td>UZA</td>
             <th scope="row">Interventie ploeg 2</th>
             <td>Beschikbaar</td>
             <td>-</td>
             <td>-</td>
             </tr>
             <tr class="table-danger">
-            <td>3</td>
+            <td>HP2</td>
             <th scope="row">Interventie ploeg 3</th>
             <td>Transport</td>
             <td>12345</td>
             <td>Jesusstreet 12</td>
             </tr>
             <tr class="table-warning">
-            <td>4</td>
+            <td>HP1</td>
             <th scope="row">Interventie ploeg 1</th>
             <td>Vertrokken</td>
             <td>123456</td>
             <td>Smallandlaan 14a, 2660 Hoboken</td>
             </tr>
             <tr class="table-success">
-            <td>5</td>
+            <td>UZA</td>
             <th scope="row">Interventie ploeg 2</th>
             <td>Beschikbaar</td>
             <td>-</td>
             <td>-</td>
             </tr>
             <tr class="table-danger">
-            <td>6</td>
+            <td>HP2</td>
             <th scope="row">Interventie ploeg 3</th>
             <td>Transport</td>
             <td>12345</td>
             <td>Jesusstreet 12</td>
-            </tr><tr class="table-warning">
-            <td>7</td>
+            </tr>
+            <tr class="table-warning">
+            <td>HP1</td>
             <th scope="row">Interventie ploeg 1</th>
             <td>Vertrokken</td>
             <td>123456</td>
             <td>Smallandlaan 14a, 2660 Hoboken</td>
             </tr>
             <tr class="table-success">
-            <td>8</td>
+            <td>UZA</td>
             <th scope="row">Interventie ploeg 2</th>
             <td>Beschikbaar</td>
             <td>-</td>
             <td>-</td>
             </tr>
             <tr class="table-danger">
-            <td>9</td>
+            <td>HP2</td>
             <th scope="row">Interventie ploeg 3</th>
             <td>Transport</td>
             <td>12345</td>
             <td>Jesusstreet 12</td>
-            </tr><tr class="table-warning">
-            <td>10</td>
+            </tr>
+            <tr class="table-warning">
+            <td>HP1</td>
             <th scope="row">Interventie ploeg 1</th>
             <td>Vertrokken</td>
             <td>123456</td>
             <td>Smallandlaan 14a, 2660 Hoboken</td>
             </tr>
             <tr class="table-success">
-            <td>11</td>
+            <td>UZA</td>
             <th scope="row">Interventie ploeg 2</th>
             <td>Beschikbaar</td>
             <td>-</td>
             <td>-</td>
             </tr>
             <tr class="table-danger">
-            <td>12</td>
+            <td>HP2</td>
             <th scope="row">Interventie ploeg 3</th>
             <td>Transport</td>
             <td>12345</td>
             <td>Jesusstreet 12</td>
             </tr>
-            
-        </tbody>
-        </table>
-        </div>
-    </div>
+            <tr class="table-warning">
+            <td>HP1</td>
+            <th scope="row">Interventie ploeg 1</th>
+            <td>Vertrokken</td>
+            <td>123456</td>
+            <td>Smallandlaan 14a, 2660 Hoboken</td>
+            </tr>
+            <tr class="table-success">
+            <td>UZA</td>
+            <th scope="row">Interventie ploeg 2</th>
+            <td>Beschikbaar</td>
+            <td>-</td>
+            <td>-</td>
+            </tr>
+            <tr class="table-danger">
+            <td>HP2</td>
+            <th scope="row">Interventie ploeg 3</th>
+            <td>Transport</td>
+            <td>12345</td>
+            <td>Jesusstreet 12</td>
+            </tr>
+            <tr class="table-warning">
+            <td>HP1</td>
+            <th scope="row">Interventie ploeg 1</th>
+            <td>Vertrokken</td>
+            <td>123456</td>
+            <td>Smallandlaan 14a, 2660 Hoboken</td>
+            </tr>
+            <tr class="table-success">
+            <td>UZA</td>
+            <th scope="row">Interventie ploeg 2</th>
+            <td>Beschikbaar</td>
+            <td>-</td>
+            <td>-</td>
+            </tr>
+            <tr class="table-danger">
+            <td>HP2</td>
+            <th scope="row">Interventie ploeg 3</th>
+            <td>Transport</td>
+            <td>12345</td>
+            <td>Jesusstreet 12</td>
+            </tr>
+            <tr class="table-warning">
+            <td>HP1</td>
+            <th scope="row">Interventie ploeg 1</th>
+            <td>Vertrokken</td>
+            <td>123456</td>
+            <td>Smallandlaan 14a, 2660 Hoboken</td>
+            </tr>
+            <tr class="table-success">
+            <td>UZA</td>
+            <th scope="row">Interventie ploeg 2</th>
+            <td>Beschikbaar</td>
+            <td>-</td>
+            <td>-</td>
+            </tr>
+            <tr class="table-danger">
+            <td>HP2</td>
+            <th scope="row">Interventie ploeg 3</th>
+            <td>Transport</td>
+            <td>12345</td>
+            <td>Jesusstreet 12</td>
+            </tr>
+            <tr class="table-warning">
+            <td>HP1</td>
+            <th scope="row">Interventie ploeg 1</th>
+            <td>Vertrokken</td>
+            <td>123456</td>
+            <td>Smallandlaan 14a, 2660 Hoboken</td>
+            </tr>
+            <tr class="table-success">
+            <td>UZA</td>
+            <th scope="row">Interventie ploeg 2</th>
+            <td>Beschikbaar</td>
+            <td>-</td>
+            <td>-</td>
+            </tr>
+            <tr class="table-danger">
+            <td>HP2</td>
+            <th scope="row">Interventie ploeg 3</th>
+            <td>Transport</td>
+            <td>12345</td>
+            <td>Jesusstreet 12</td>
+            </tr>
 
-    <div class="row">
-        <div class="col-2">
-            <div class="row">
-                <div class=col-12>
-                    <div class="card-header"  id="category">Hulppost 1</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-10">
-        <table class="table">
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            </tr>
         </tbody>
         </table>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-2">
-            <div class="row">
-                <div class=col-12>
-                    <div class="card-header"  id="category">Hulppost 1</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-10">
-        <table class="table">
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            </tr>
-        </tbody>
-        </table>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-2">
-            <div class="row">
-                <div class=col-12>
-                    <div class="card-header"  id="category">Hulppost 1</div>
-                </div>
-            </div>
-        </div>
-        <div class="col-10">
-        <table class="table">
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            </tr>
-        </tbody>
-        </table>
-        </div>
     </div>
 
 
