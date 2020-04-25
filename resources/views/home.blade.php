05b2bc0023d9225e3b2465a68fefb42ac6b11fe9
Noah Gillard <noah.gillard@student.kdg.be>
2020-04-25 13:59:55 +0200

    change colors header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,8 +26,45 @@
     </div>
 
     <div class="row">
-        <div class="col">       
-            <div class="card-header">
+        <div class="col">
+        <div class="card border-success">       
+            <div class="card-header bg-success">
+                <a>Interventie ploeg 1</a>
+                <a id="clock">09:55</a>
+            </div>
+                <div class="card-body border">
+                    <div class="row">
+                        <div class="col-4">
+                            <img class="img-thumbnail" src="images/icons/medical-profile-icon.png"></img>
+                            <div class="btn-group">
+                                        <button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Status</button>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">Beschikbaar</a>
+                                            <a class="dropdown-item" href="#">Vertrokken</a>
+                                            <a class="dropdown-item" href="#">Ter Plaatse</a>
+                                            <a class="dropdown-item" href="#">Transport</a>
+                                            <a class="dropdown-item" href="#">Transport aankomst</a>
+                                            <a class="dropdown-item" href="#">Radiofonisch</a>
+                                                <div class="dropdown-divider">
+                                                </div>
+                                            <a class="dropdown-item" href="#">Niet Beschikbaar</a>
+                                        </div>
+                                    </div>
+                        </div>
+                        <div class="col" id="badge_col-2">
+                            <div class="row">
+                                <div class="form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                </div> 
+            </div>
+            <div class="col">    
+            <div class="card border-danger">    
+            <div class="card-header bg-danger">
                 <a>Interventie ploeg 1</a>
                 <a id="clock">09:55</a>
             </div>
@@ -59,40 +96,7 @@
                         </div>
                     </div>
                 </div>  
-            </div>
-            <div class="col">       
-            <div class="card-header">
-                <a>Interventie ploeg 1</a>
-                <a id="clock">09:55</a>
-            </div>
-                <div class="card-body border">
-                    <div class="row">
-                        <div class="col-4">
-                            <img class="img-thumbnail" src="images/icons/medical-profile-icon.png"></img>
-                            <div class="btn-group">
-                                        <button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Status</button>
-                                        <div class="dropdown-menu">
-                                            <a class="dropdown-item" href="#">Beschikbaar</a>
-                                            <a class="dropdown-item" href="#">Vertrokken</a>
-                                            <a class="dropdown-item" href="#">Ter Plaatse</a>
-                                            <a class="dropdown-item" href="#">Transport</a>
-                                            <a class="dropdown-item" href="#">Transport aankomst</a>
-                                            <a class="dropdown-item" href="#">Radiofonisch</a>
-                                                <div class="dropdown-divider">
-                                                </div>
-                                            <a class="dropdown-item" href="#">Niet Beschikbaar</a>
-                                        </div>
-                                    </div>
-                        </div>
-                        <div class="col" id="badge_col-2">
-                            <div class="row">
-                                <div class="form-group">
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>  
+                </div> 
             </div>
             <div class="col">       
             <div class="card-header">
